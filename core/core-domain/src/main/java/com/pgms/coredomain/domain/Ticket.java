57fc57c8feb8694dd11df5a93bfa36d2b8d40d77
Hanna <hanna@trdst.com>
2023-12-22 11:46:26 +0900

    feat: order, ticket 엔티티 정의

---
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/Ticket.java
@@ -0,0 +1,24 @@
+package com.pgms.coredomain.domain;
+
+import com.pgms.coredomain.domain.common.BaseEntity;
+import com.pgms.coredomain.domain.order.Order;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ticket")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ticket extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Order order;
+
+    //TODO: 공연 좌석 매핑
+}
---
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/order/Order.java
@@ -0,0 +1,53 @@
+package com.pgms.coredomain.domain.order;
+
+import com.pgms.coredomain.domain.Ticket;
+import com.pgms.coredomain.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "order")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "order_name", nullable = false)
+    private String orderName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "receipt_type", nullable = false)
+    private ReceiptType receiptType;
+
+    @Column(name = "buyer_name", nullable = false)
+    private String buyerName;
+
+    @Column(name = "buyer_phone_number", nullable = false)
+    private String buyerPhoneNumber;
+
+    @Column(name = "street_address")
+    private String streetAddress;
+
+    @Column(name = "detail_address")
+    private String detailAddress;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @Column(name = "amount", nullable = false)
+    private int amount;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ticket> tickets = new ArrayList<>();
+}
