85896dc55b07e04303dd5046e7558351bd059362
KarmaPol <kkhkr98@gmail.com>
2023-12-22 09:30:22 +0900

    feat : baseentity 추가, user 모듈명 변경 -> member

--- core/core-domain/src/main/java/com/pgms/coredomain/CoreDomainConfig.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/CoreDomainConfig.java
@@ -2,6 +2,7 @@ package com.pgms.coredomain;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,5 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan("com.pgms.coredomain")
 @EnableJpaRepositories("com.pgms.coredomain")
+@EnableJpaAuditing
 public class CoreDomainConfig {
 }
---
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.pgms.coredomain.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
