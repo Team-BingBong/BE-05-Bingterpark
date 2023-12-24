69b70fe92d894394e1467d918f54eb787d57a7f4
park0jae <okvv26@naver.com>
2023-12-24 16:13:31 +0900

    fix: #12 EventEdit 클래스 내의 @Getter 어노테이션으로 인한 오류 수정

--- core/core-domain/src/main/java/com/pgms/coredomain/domain/event/EventEdit.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/event/EventEdit.java
@@ -3,9 +3,7 @@ package com.pgms.coredomain.domain.event;
 import java.time.LocalDateTime;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
 public record EventEdit(
 	String title,
