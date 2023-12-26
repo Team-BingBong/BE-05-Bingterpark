cca6a1a355b3296f0a30d8dfbba27cbd1864e11d
byulcode <bybit0408@gmail.com>
2023-12-26 11:26:24 +0900

    feat: 결제 생성/승인 요청 성공 기능 구현 (#19)
    * refactor: 예매자 변수명 변경 buyer -> customer
    
    * chore : tosspayments 관련키, url 설정
    
    * chore : tosspayments 관련키, url 설정
    
    * feat: 결제 승인 요청 성공 기능 구현
    
    * feat: 결제 요청 뷰 생성
    
    * order 테이블명 임시 이스케이프 처리
    
    * refactor: tossPayment 관련 설정 config로 분리
    
    * refactor: 메서드 네이밍 동사+명사 형태로 변경
    
    * chore: 시크릿키 임의의 값으로 대체

---
+++ api/api-booking/src/main/java/com/pgms/apibooking/dto/request/PaymentCreateRequest.java
@@ -0,0 +1,21 @@
+package com.pgms.apibooking.dto.request;
+
+import com.pgms.coredomain.domain.booking.Booking;
+import com.pgms.coredomain.domain.booking.Payment;
+import com.pgms.coredomain.domain.booking.PaymentMethod;
+import com.pgms.coredomain.domain.booking.PaymentStatus;
+
+public record PaymentCreateRequest(
+	PaymentMethod method,
+	Long bookingId,
+	int amount
+) {
+	public Payment toEntity(Booking booking) {
+		return Payment.builder()
+			.method(method)
+			.booking(booking)
+			.amount(amount)
+			.status(PaymentStatus.DEPOSIT_PENDING)
+			.build();
+	}
+}
