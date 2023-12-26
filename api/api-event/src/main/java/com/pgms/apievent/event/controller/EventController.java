8e5dfbff27f6efb0d9111aac7d20c8156ba213b1
KarmaPol <kkhkr98@gmail.com>
2023-12-26 11:54:37 +0900

    feat: #38 add EventSeatArea service, controller code

--- api/api-event/src/main/java/com/pgms/apievent/event/controller/EventController.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/controller/EventController.java
@@ -1,26 +1,25 @@
 package com.pgms.apievent.event.controller;
 
-import java.net.URI;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.pgms.apievent.event.dto.request.EventCreateRequest;
+import com.pgms.apievent.event.dto.request.EventSeatAreaCreateRequest;
+import com.pgms.apievent.event.dto.request.EventSeatAreaUpdateRequest;
 import com.pgms.apievent.event.dto.request.EventUpdateRequest;
 import com.pgms.apievent.event.dto.response.EventResponse;
+import com.pgms.apievent.event.dto.response.EventSeatAreaResponse;
 import com.pgms.apievent.event.service.EventService;
 import com.pgms.coredomain.response.ApiResponse;
+<<<<<<< Updated upstream
 
 import jakarta.validation.Valid;
+=======
+>>>>>>> Stashed changes
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -56,6 +55,32 @@ public class EventController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteEventById(@PathVariable Long id) {
 		eventService.deleteEventById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PostMapping("/{id}/seat-area")
+	public ResponseEntity<ApiResponse> createEventSeatArea(@PathVariable Long id,
+														   @RequestBody EventSeatAreaCreateRequest request){
+		List<EventSeatAreaResponse> response = eventService.createEventSeatArea(id, request);
+		return ResponseEntity.ok(ApiResponse.ok(response));
+	}
+
+	@GetMapping("/{id}/seat-area")
+	public ResponseEntity<ApiResponse> getEventSeatAreas(@PathVariable Long id){
+		List<EventSeatAreaResponse> response = eventService.getEventSeatAreas(id);
+		return ResponseEntity.ok(ApiResponse.ok(response));
+	}
+
+	@DeleteMapping("seat-area/{areaId}")
+	public ResponseEntity<Void> deleteEventSeatArea(@PathVariable Long areaId){
+		eventService.deleteEventSeatArea(areaId);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("seat-area/{areaId}")
+	public ResponseEntity<Void> updateEventSeatArea(@PathVariable Long areaId,
+													@RequestBody EventSeatAreaUpdateRequest request){
+		eventService.updateEventSeatArea(areaId, request);
 		return ResponseEntity.noContent().build();
 	}
 }
