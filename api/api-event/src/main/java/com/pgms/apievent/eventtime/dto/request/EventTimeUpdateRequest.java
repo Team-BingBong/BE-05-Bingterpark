bd64641149cce0bbda8ac8ca897d4e02e07d8d66
park0jae <okvv26@naver.com>
2023-12-26 11:24:15 +0900

    feat: #32 EventTime DTO 객체 Validation 추가

--- api/api-event/src/main/java/com/pgms/apievent/eventtime/dto/request/EventTimeUpdateRequest.java
+++ api/api-event/src/main/java/com/pgms/apievent/eventtime/dto/request/EventTimeUpdateRequest.java
@@ -2,8 +2,13 @@ package com.pgms.apievent.eventtime.dto.request;
 
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public record EventTimeUpdateRequest(
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	LocalDateTime startTime,
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	LocalDateTime endTime
 ) {
 }
