f1ba48c92e864345a8c867e14e7e41529fc4b30c
Hanna <hanna@trdst.com>
2023-12-27 20:31:40 +0900

    feat: Booking에 Member ManyToOne 매핑 추가

--- core/core-domain/src/main/java/com/pgms/coredomain/domain/booking/Booking.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/booking/Booking.java
@@ -5,15 +5,21 @@ import java.util.List;
 
 import com.pgms.coredomain.domain.common.BaseEntity;
 import com.pgms.coredomain.domain.event.Ticket;
+import com.pgms.coredomain.domain.member.Member;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -61,10 +67,13 @@ public class Booking extends BaseEntity {
 	@Column(name = "amount", nullable = false)
 	private int amount;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private Member member;
+
 	@OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
 	private List<Ticket> tickets = new ArrayList<>();
 
 	@OneToOne(mappedBy = "booking", cascade = CascadeType.ALL)
 	private Payment payment;
-	//TODO: 회원 매핑
 }
