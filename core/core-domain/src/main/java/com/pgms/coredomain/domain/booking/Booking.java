daa61fa0da6ce66970d7080e25185b9608025bf1
Hanna <hanna@trdst.com>
2023-12-28 21:06:44 +0900

    feat: Booking id String 타입으로 변경, 수령자 이름과 휴대폰 번호 추가

--- core/core-domain/src/main/java/com/pgms/coredomain/domain/booking/Booking.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/booking/Booking.java
@@ -15,8 +15,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -24,6 +22,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -34,9 +33,8 @@ import lombok.NoArgsConstructor;
 public class Booking extends BaseEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
-	private Long id;
+	private String id;
 
 	@Column(name = "booking_name", nullable = false)
 	private String bookingName;
@@ -55,6 +53,12 @@ public class Booking extends BaseEntity {
 	@Column(name = "buyer_phone_number", nullable = false)
 	private String buyerPhoneNumber;
 
+	@Column(name = "recipient_name")
+	private String recipientName;
+
+	@Column(name = "recipient_phone_number")
+	private String recipientPhoneNumber;
+
 	@Column(name = "street_address")
 	private String streetAddress;
 
@@ -65,10 +69,10 @@ public class Booking extends BaseEntity {
 	private String zipCode;
 
 	@Column(name = "amount", nullable = false)
-	private int amount;
+	private Integer amount;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	@JoinColumn(name = "member_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)) //TODO: nullalbe = false 추가
 	private Member member;
 
 	@OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
@@ -76,4 +80,35 @@ public class Booking extends BaseEntity {
 
 	@OneToOne(mappedBy = "booking", cascade = CascadeType.ALL)
 	private Payment payment;
+
+	@Builder
+	public Booking(
+		String id,
+		String bookingName,
+		BookingStatus status,
+		ReceiptType receiptType,
+		String buyerName,
+		String buyerPhoneNumber,
+		String recipientName,
+		String recipientPhoneNumber,
+		String streetAddress,
+		String detailAddress,
+		String zipCode,
+		Integer amount,
+		Member member
+	) {
+		this.id = id;
+		this.bookingName = bookingName;
+		this.status = status;
+		this.receiptType = receiptType;
+		this.buyerName = buyerName;
+		this.buyerPhoneNumber = buyerPhoneNumber;
+		this.recipientName = recipientName;
+		this.recipientPhoneNumber = recipientPhoneNumber;
+		this.streetAddress = streetAddress;
+		this.detailAddress = detailAddress;
+		this.zipCode = zipCode;
+		this.amount = amount;
+		this.member = member;
+	}
 }
