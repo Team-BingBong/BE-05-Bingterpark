09b40c4c6b02c0746b45505f3f9acf42aff09792
marooo326 <ktnqkr01@gmail.com>
2023-12-28 13:49:48 +0900

    feat: Role 관련 fetch join 메서드 및 EntityGraph 적용

--- core/core-domain/src/main/java/com/pgms/coredomain/domain/member/repository/MemberRepository.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/member/repository/MemberRepository.java
@@ -1,13 +1,21 @@
 package com.pgms.coredomain.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pgms.coredomain.domain.member.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	boolean existsByRoleId(Long roleId);
+
+	@EntityGraph(attributePaths = {"role"})
 	Slice<Member> findSliceBy(Pageable pageable);
 
-	boolean existsByRoleId(Long roleId);
+	@Query("select m from Member m join fetch m.role r where m.id = :id")
+	Optional<Member> findByIdWithRole(Long id);
 }
