9fe16132f1c2eb2baa8c6f8f9074e483b9217da4
zerozae <84398970+park0jae@users.noreply.github.com>
2023-12-30 20:17:44 +0900

    feat: 공연 이미지 관리 기능 구현 (#79)
    * feat: #56 EventImageRepository 생성 및 Event & EventTime 내의 날짜관련 컬럼명 수정(date -> At)
    
    * feat: #56 S3 관련 의존성 추가 및 yml 설정 & S3Config 파일 설정 추가
    
    * feat: #56 ImageUtil 클래스 추가
    
    * feat: #56 Image 관련 DTO 클래스 구현
    
    * feat: #56 S3Service & EventImageService 구현
    
    * feat: #56 EventImageController 구현
    
    * feat: #56 공연 도메인쪽에 이미지 로직 추가
    
    * refactor: #56 일부 코드 리팩토링
    
    * fix: #56 data.sql 파일 내의 변경된 Event 날짜 관련 컬럼 적용 및 Thumbnail update 로직 분리
    
    * feat: #56 EventImageController 엔드포인트 수정 및 Thumbnail 부분 분리
    
    ---------
    
    Co-authored-by: Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>

--- core/core-domain/src/main/java/com/pgms/coredomain/domain/event/EventImage.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/event/EventImage.java
@@ -1,29 +1,44 @@
 package com.pgms.coredomain.domain.event;
 
 import com.pgms.coredomain.domain.common.BaseEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "event_image")
 public class EventImage extends BaseEntity {
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Lob
-    @Column(name = "url")
-    private String url;
+	@Lob
+	@Column(name = "url")
+	private String url;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "event_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private Event event;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "event_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private Event event;
+
+	public EventImage(String url, Event event) {
+		this.url = url;
+		this.event = event;
+	}
 }
