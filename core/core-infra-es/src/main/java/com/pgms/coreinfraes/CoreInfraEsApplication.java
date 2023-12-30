565c26d4a8350f881587cc837b739b583dcbab4e
KarmaPol <kkhkr98@gmail.com>
2023-12-30 21:14:40 +0900

    chore: #84 spring elastic data 그래들 설정

---
+++ core/core-infra-es/src/main/java/com/pgms/coreinfraes/CoreInfraEsApplication.java
@@ -0,0 +1,13 @@
+package com.pgms.coreinfraes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreInfraEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreInfraEsApplication.class, args);
+    }
+
+}
---
+++ core/core-infra-es/src/test/java/com/pgms/coreinfraes/CoreInfraEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgms.coreinfraes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreInfraEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
