167f832ede5e8301c6d77b8ba2d73a7195e08c55
ebcho <joeunbi514@naver.com>
2024-01-02 11:17:42 +0900

    refactor: SecurityConfig 빈 삭제, filterChain 리팩터링

--- api/api-member/src/main/java/com/pgms/apimember/security/WebSecurityConfig.java
+++ api/api-member/src/main/java/com/pgms/apimember/security/WebSecurityConfig.java
@@ -3,11 +3,11 @@ package com.pgms.apimember.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,8 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.pgms.apimember.security.jwt.JwtAuthenticationEntryPoint;
 import com.pgms.apimember.security.jwt.JwtAuthenticationFilter;
-import com.pgms.apimember.security.jwt.JwtUtils;
-import com.pgms.apimember.security.service.UserDetailsServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,23 +24,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-	private final UserDetailsServiceImpl userDetailsService;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-	private final JwtUtils jwtUtils;
 
 	@Bean
-	public JwtAuthenticationFilter authenticationJwtTokenFilter() {
-		return new JwtAuthenticationFilter(jwtUtils, userDetailsService);
-	}
-
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(passwordEncoder());
-
-		return authProvider;
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
@@ -51,30 +38,28 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(AbstractHttpConfigurer::disable)
-			.exceptionHandling(exception -> {
-				exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
-				// exception.accessDeniedHandler(customAccessDeniedHandler()); // TODO AccessDeniedHandler 추가
-			})
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.cors(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
 			.authorizeHttpRequests(auth ->
 				// TODO authorize 설정
 				// auth.requestMatchers("/api/auth/**").permitAll()
 				// 	.requestMatchers("/api/test/**").permitAll()
 				auth.anyRequest().permitAll()
 			)
-			.headers().frameOptions().sameOrigin();
-
-		http.authenticationProvider(authenticationProvider());
-
-		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+			.exceptionHandling(exception -> {
+				exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+				// exception.accessDeniedHandler(customAccessDeniedHandler()); // TODO AccessDeniedHandler 추가
+			})
+			.addFilterBefore(jwtAuthenticationFilter,
+				UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
