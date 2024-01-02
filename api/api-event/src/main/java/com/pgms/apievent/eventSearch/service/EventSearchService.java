8723c7900dd60aed348b86ef3526f45a8dada139
park0jae <okvv26@naver.com>
2024-01-02 13:46:12 +0900

    feat: #97 EventSearchService내의 검색 기능 로직 추가

--- api/api-event/src/main/java/com/pgms/apievent/eventSearch/service/EventSearchService.java
+++ api/api-event/src/main/java/com/pgms/apievent/eventSearch/service/EventSearchService.java
@@ -1,22 +1,27 @@
 package com.pgms.apievent.eventSearch.service;
 
-import com.pgms.coreinfraes.document.EventDocument;
-import com.pgms.coreinfraes.dto.request.EventSearchRequest;
-import com.pgms.coreinfraes.repository.EventSearchQueryRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.pgms.apievent.common.dto.response.PageResponseDto;
+import com.pgms.apievent.eventSearch.dto.request.EventSearchRequest;
+import com.pgms.coreinfraes.document.EventDocument;
+import com.pgms.coreinfraes.dto.EventSearchDto;
+import com.pgms.coreinfraes.repository.EventSearchQueryRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class EventSearchService {
 
-    private final EventSearchQueryRepository eventSearchQueryRepository;
+	private final EventSearchQueryRepository eventSearchQueryRepository;
 
-    public void searchEvents(EventSearchRequest eventSearchRequest) {
-
-        List<EventDocument> eventDocuments = eventSearchQueryRepository.findByCondition(eventSearchRequest);
-
-    }
+	@Transactional(readOnly = true)
+	public PageResponseDto searchEvents(EventSearchRequest eventSearchRequest) {
+		EventSearchDto eventSearchDto = eventSearchRequest.toDto();
+		Page<EventDocument> eventDocuments = eventSearchQueryRepository.findByCondition(eventSearchDto);
+		return PageResponseDto.of(eventDocuments);
+	}
 }
