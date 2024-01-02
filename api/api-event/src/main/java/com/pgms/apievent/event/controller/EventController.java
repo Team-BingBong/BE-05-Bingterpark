92a1ccaa1b78433596530e2128912e9e9c5db992
park0jae <okvv26@naver.com>
2024-01-02 13:46:25 +0900

    feat: #97 EventSearchController 구현 및 일부사항 수정

--- api/api-event/src/main/java/com/pgms/apievent/event/controller/EventController.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/controller/EventController.java
@@ -3,8 +3,6 @@ package com.pgms.apievent.event.controller;
 import java.net.URI;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.pgms.apievent.common.dto.response.PageResponse;
 import com.pgms.apievent.event.dto.request.EventCreateRequest;
 import com.pgms.apievent.event.dto.request.EventSeatAreaCreateRequest;
 import com.pgms.apievent.event.dto.request.EventSeatAreaUpdateRequest;
@@ -26,7 +23,6 @@ import com.pgms.apievent.event.dto.response.EventResponse;
 import com.pgms.apievent.event.dto.response.EventSeatAreaResponse;
 import com.pgms.apievent.event.service.EventService;
 import com.pgms.coredomain.response.ApiResponse;
-import com.pgms.coreinfraes.document.EventDocument;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -52,12 +48,6 @@ public class EventController {
 	public ResponseEntity<ApiResponse> getEventById(@PathVariable Long id) {
 		EventResponse response = eventService.getEventById(id);
 		return ResponseEntity.ok(ApiResponse.ok(response));
-	}
-
-	@GetMapping("/search/{title}")
-	public ResponseEntity<ApiResponse> searchEventByTitle(@PathVariable String title, Pageable pageable) {
-		Page<EventDocument> eventDocuments = eventService.searchByTitle(title, pageable);
-		return ResponseEntity.ok(ApiResponse.ok(PageResponse.of(eventDocuments)));
 	}
 
 	@PutMapping("/{id}")
--- api/api-event/src/main/java/com/pgms/apievent/event/service/EventService.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/service/EventService.java
@@ -1,6 +1,12 @@
 package com.pgms.apievent.event.service;
 
-import com.pgms.apievent.common.dto.response.PageResponse;
+import static com.pgms.apievent.exception.EventErrorCode.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.pgms.apievent.event.dto.request.EventCreateRequest;
 import com.pgms.apievent.event.dto.request.EventSeatAreaCreateRequest;
 import com.pgms.apievent.event.dto.request.EventSeatAreaUpdateRequest;
@@ -17,18 +23,9 @@ import com.pgms.coredomain.domain.event.repository.EventHallRepository;
 import com.pgms.coredomain.domain.event.repository.EventRepository;
 import com.pgms.coredomain.domain.event.repository.EventSeatAreaRepository;
 import com.pgms.coreinfraes.document.EventDocument;
-import com.pgms.coreinfraes.repository.EventSearchQueryRepository;
 import com.pgms.coreinfraes.repository.EventSearchRepository;
+
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.pgms.apievent.exception.EventErrorCode.*;
 
 @Service
 @Transactional
@@ -39,7 +36,6 @@ public class EventService {
 	private final EventHallRepository eventHallRepository;
 	private final EventSeatAreaRepository eventSeatAreaRepository;
 	private final EventSearchRepository eventSearchRepository;
-	private final EventSearchQueryRepository eventSearchQueryRepository;
 
 	public EventResponse createEvent(EventCreateRequest request) {
 		EventHall eventHall = getEventHall(request.eventHallId());
@@ -56,11 +52,6 @@ public class EventService {
 	public EventResponse getEventById(Long id) {
 		Event event = getEvent(id);
 		return EventResponse.of(event);
-	}
-
-	@Transactional(readOnly = true)
-	public Page<EventDocument> searchByTitle(String title, Pageable pageable) {
-		return eventSearchQueryRepository.searchByEventTitle(title, pageable);
 	}
 
 	public EventResponse updateEvent(Long id, EventUpdateRequest request) {
