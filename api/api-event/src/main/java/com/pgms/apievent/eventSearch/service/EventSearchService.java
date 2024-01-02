987c8559071c904256814faf8ed1262bf5094bcf
KarmaPol <kkhkr98@gmail.com>
2024-01-02 11:30:44 +0900

    feat: #98 공연 검색 Controller, Service 추가, http 문서 추가

--- api/api-event/src/main/java/com/pgms/apievent/eventSearch/service/EventSearchService.java
+++ api/api-event/src/main/java/com/pgms/apievent/eventSearch/service/EventSearchService.java
@@ -1,17 +1,22 @@
 package com.pgms.apievent.eventSearch.service;
 
+import com.pgms.coreinfraes.document.EventDocument;
 import com.pgms.coreinfraes.dto.request.EventSearchRequest;
-import com.pgms.coreinfraes.repository.EventSearchRepository;
+import com.pgms.coreinfraes.repository.EventSearchQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventSearchService {
 
-    private final EventSearchRepository eventSearchRepository;
+    private final EventSearchQueryRepository eventSearchQueryRepository;
 
     public void searchEvents(EventSearchRequest eventSearchRequest) {
+
+        List<EventDocument> eventDocuments = eventSearchQueryRepository.findByCondition(eventSearchRequest);
 
     }
 }
