1d65527f51c080ac1e1b8fee1558e3c7a5ced589
ebcho <joeunbi514@naver.com>
2024-01-04 13:57:51 +0900

    feat: 현재 로그인한 계정 정보 CurrentAccount 추가

---
+++ core/core-security/src/main/java/com/pgms/coresecurity/security/resolver/CurrentAccountArgumentResolver.java
@@ -0,0 +1,38 @@
+package com.pgms.coresecurity.security.resolver;
+
+import java.util.Objects;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.pgms.coresecurity.security.service.UserDetailsImpl;
+
+public class CurrentAccountArgumentResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		boolean hasParameterAnnotation = parameter.hasParameterAnnotation(CurrentAccount.class);
+		boolean hasLongParameterType = parameter.getParameterType().isAssignableFrom(Long.class);
+		return hasParameterAnnotation & hasLongParameterType;
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		checkAuthenticated(authentication);
+		UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
+		return userDetails.getId();
+	}
+
+	private void checkAuthenticated(Authentication authentication) {
+		if (Objects.isNull(authentication)) {
+			throw new RuntimeException("인증되지 않은 요청입니다.");
+		}
+	}
+}
