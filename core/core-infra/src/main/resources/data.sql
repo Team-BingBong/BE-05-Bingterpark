69f266482c9eced3d14c8eb3b1b09a911a9f273f
marooo326 <ktnqkr01@gmail.com>
2024-01-04 17:55:18 +0900

    docs: data.sql에 Guest Role 추가

--- core/core-infra/src/main/resources/data.sql
+++ core/core-infra/src/main/resources/data.sql
@@ -42,7 +42,8 @@ VALUES ('E3', 'BEING_BOOKED', 2, 1);
 INSERT INTO role (name)
 VALUES ('ROLE_SUPERADMIN'),
        ('ROLE_ADMIN'),
-       ('ROLE_USER');
+       ('ROLE_USER'),
+       ('ROLE_GUEST');
 
 
 -- Member
@@ -62,6 +63,6 @@ VALUES ('슈퍼관리자', 'superadmin@example.com', '$2a$10$tfdM.PjviEH0zMEXVYj
        ('관리자', 'admin@example.com', '$2a$10$tfdM.PjviEH0zMEXVYjH.ODJPSviQRrYpb17rdMjvJtbWbSnC8nTa', '01012345678',
         'ACTIVE', 2, NOW(), NOW()),
        ('오래된 슈퍼관리자', 'oldSuperadmin@example.com', '$2a$10$tfdM.PjviEH0zMEXVYjH.ODJPSviQRrYpb17rdMjvJtbWbSnC8nTa',
-        '01012345678','ACTIVE', 1, '2020-01-01', NOW());
+        '01012345678', 'ACTIVE', 1, '2020-01-01', NOW());
 
 
