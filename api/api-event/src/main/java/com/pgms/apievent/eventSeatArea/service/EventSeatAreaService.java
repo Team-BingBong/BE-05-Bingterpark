2a6540cb86dfc815fd4f628e64f2a00b01b42544
park0jae <okvv26@naver.com>
2024-01-05 16:19:50 +0900

    refactor: #128 EventSeatArea 관련 로직 기존에 Event 부분에 작성된 부분을 -> EventSeatArea로 따로 분리함

---
+++ api/api-event/src/main/java/com/pgms/apievent/eventSeatArea/service/EventSeatAreaService.java
@@ -0,0 +1,73 @@
+package com.pgms.apievent.eventSeatArea.service;
+
+import static com.pgms.apievent.exception.EventErrorCode.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pgms.apievent.eventSeatArea.dto.request.EventSeatAreaCreateRequest;
+import com.pgms.apievent.eventSeatArea.dto.request.EventSeatAreaUpdateRequest;
+import com.pgms.apievent.eventSeatArea.dto.response.EventSeatAreaResponse;
+import com.pgms.apievent.exception.CustomException;
+import com.pgms.apievent.exception.EventSeatAreaNotFoundException;
+import com.pgms.coredomain.domain.event.Event;
+import com.pgms.coredomain.domain.event.EventSeatArea;
+import com.pgms.coredomain.domain.event.repository.EventRepository;
+import com.pgms.coredomain.domain.event.repository.EventSeatAreaRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EventSeatAreaService {
+
+	private final EventRepository eventRepository;
+	private final EventSeatAreaRepository eventSeatAreaRepository;
+
+	public List<EventSeatAreaResponse> createEventSeatArea(Long id, EventSeatAreaCreateRequest request) {
+		Event event = getEvent(id);
+
+		List<EventSeatArea> eventSeatAreas = request.requests().stream()
+			.map(areaRequest -> new EventSeatArea(areaRequest.seatAreaType(), areaRequest.price(), event))
+			.toList();
+
+		List<EventSeatArea> savedEventSeatAreas = eventSeatAreaRepository.saveAll(eventSeatAreas);
+
+		return savedEventSeatAreas.stream()
+			.map(EventSeatAreaResponse::of)
+			.toList();
+	}
+
+	public void deleteEventSeatArea(Long areaId) {
+		EventSeatArea eventSeatArea = getEventSeatArea(areaId);
+		eventSeatAreaRepository.delete(eventSeatArea);
+	}
+
+	public void updateEventSeatArea(Long areaId, EventSeatAreaUpdateRequest request) {
+		EventSeatArea eventSeatArea = getEventSeatArea(areaId);
+		eventSeatArea.updateEventSeatAreaPriceAndType(request.seatAreaType(), request.price());
+	}
+
+	@Transactional(readOnly = true)
+	public List<EventSeatAreaResponse> getEventSeatAreas(Long id) {
+		Event event = getEvent(id);
+		List<EventSeatArea> eventSeatAreas = eventSeatAreaRepository.findEventSeatAreasByEvent(event);
+
+		return eventSeatAreas.stream()
+			.map(EventSeatAreaResponse::of)
+			.toList();
+	}
+
+	private Event getEvent(Long eventId) {
+		return eventRepository.findById(eventId)
+			.orElseThrow(() -> new CustomException(EVENT_NOT_FOUND));
+	}
+
+	private EventSeatArea getEventSeatArea(Long areaId) {
+		return eventSeatAreaRepository.findById(areaId)
+			.orElseThrow(EventSeatAreaNotFoundException::new);
+	}
+}
