2d7a75baa200f4ae33b24b2b22af8b060c5106a3
zerozae <84398970+park0jae@users.noreply.github.com>
2024-01-06 20:54:52 +0900

    feat: 공연 목록 조회 구현(Querydsl) 및 MySQL 설정 (#138)
    * chore: #128 루트 docker-compose.yml 파일내의 MySQL & Redis 설정 추가 및 prod Profile의 MySQL설정 yml파일 내의 추가
    
    * refactor: #128 EventSeatArea 관련 로직 기존에 Event 부분에 작성된 부분을 -> EventSeatArea로 따로 분리함
    
    * feat: #128 공연 목록 조회 QueryDsl로 구현 (+ 랭킹순, 후기순, 마감날짜순)
    
    * chore: #128 .gitignore 내의 불필요한 폴더(data) 추가
    
    ---------
    
    Co-authored-by: Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>

--- api/api-event/src/main/java/com/pgms/apievent/event/service/EventService.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/service/EventService.java
@@ -1,30 +1,28 @@
 package com.pgms.apievent.event.service;
 
-import com.pgms.apievent.event.dto.request.EventCreateRequest;
-import com.pgms.apievent.event.dto.request.EventSeatAreaCreateRequest;
-import com.pgms.apievent.event.dto.request.EventSeatAreaUpdateRequest;
-import com.pgms.apievent.event.dto.request.EventUpdateRequest;
-import com.pgms.apievent.event.dto.response.EventResponse;
-import com.pgms.apievent.event.dto.response.EventSeatAreaResponse;
-import com.pgms.apievent.exception.CustomException;
-import com.pgms.apievent.exception.EventSeatAreaNotFoundException;
-import com.pgms.coredomain.domain.event.Event;
-import com.pgms.coredomain.domain.event.EventEdit;
-import com.pgms.coredomain.domain.event.EventHall;
-import com.pgms.coredomain.domain.event.EventSeatArea;
-import com.pgms.coredomain.domain.event.repository.EventHallRepository;
-import com.pgms.coredomain.domain.event.repository.EventRepository;
-import com.pgms.coredomain.domain.event.repository.EventSeatAreaRepository;
-import com.pgms.coreinfraes.buffer.DocumentBuffer;
-import com.pgms.coreinfraes.document.EventDocument;
-import com.pgms.coreinfraes.repository.EventSearchRepository;
-import lombok.RequiredArgsConstructor;
+import static com.pgms.apievent.exception.EventErrorCode.*;
+
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.pgms.apievent.common.dto.response.PageResponseDto;
+import com.pgms.apievent.event.dto.request.EventCreateRequest;
+import com.pgms.apievent.event.dto.request.EventPageRequest;
+import com.pgms.apievent.event.dto.request.EventUpdateRequest;
+import com.pgms.apievent.event.dto.response.EventResponse;
+import com.pgms.apievent.event.repository.EventCustomRepository;
+import com.pgms.apievent.exception.CustomException;
+import com.pgms.coredomain.domain.event.Event;
+import com.pgms.coredomain.domain.event.EventEdit;
+import com.pgms.coredomain.domain.event.EventHall;
+import com.pgms.coredomain.domain.event.repository.EventHallRepository;
+import com.pgms.coredomain.domain.event.repository.EventRepository;
+import com.pgms.coreinfraes.buffer.DocumentBuffer;
+import com.pgms.coreinfraes.document.EventDocument;
+import com.pgms.coreinfraes.repository.EventSearchRepository;
 
-import static com.pgms.apievent.exception.EventErrorCode.*;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
@@ -33,8 +31,8 @@ public class EventService {
 
 	private final EventRepository eventRepository;
 	private final EventHallRepository eventHallRepository;
-	private final EventSeatAreaRepository eventSeatAreaRepository;
 	private final EventSearchRepository eventSearchRepository;
+	private final EventCustomRepository eventCustomRepository;
 
 	public EventResponse createEvent(EventCreateRequest request) {
 		EventHall eventHall = getEventHall(request.eventHallId());
@@ -51,6 +49,24 @@ public class EventService {
 	public EventResponse getEventById(Long id) {
 		Event event = getEvent(id);
 		return EventResponse.of(event);
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponseDto getEventsPageByGenreSortedByRanking(EventPageRequest request) {
+		Page<EventResponse> events = eventCustomRepository.getEventsPageByGenreSortedByRanking(request);
+		return PageResponseDto.of(events);
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponseDto getEventsPageByGenreSortedByReview(EventPageRequest request) {
+		Page<EventResponse> events = eventCustomRepository.getEventsPageByGenreSortedByReview(request);
+		return PageResponseDto.of(events);
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponseDto getEventsPageByGenreSortedByBookingEndedAt(EventPageRequest request) {
+		Page<EventResponse> events = eventCustomRepository.getEventsPageByGenreSortedByBookingEndedAt(request);
+		return PageResponseDto.of(events);
 	}
 
 	public EventResponse updateEvent(Long id, EventUpdateRequest request) {
@@ -91,40 +107,6 @@ public class EventService {
 			.build();
 	}
 
-	public List<EventSeatAreaResponse> createEventSeatArea(Long id, EventSeatAreaCreateRequest request) {
-		Event event = getEvent(id);
-
-		List<EventSeatArea> eventSeatAreas = request.requests().stream()
-			.map(areaRequest -> new EventSeatArea(areaRequest.seatAreaType(), areaRequest.price(), event))
-			.toList();
-
-		List<EventSeatArea> savedEventSeatAreas = eventSeatAreaRepository.saveAll(eventSeatAreas);
-
-		return savedEventSeatAreas.stream()
-			.map(EventSeatAreaResponse::of)
-			.toList();
-	}
-
-	public void deleteEventSeatArea(Long areaId) {
-		EventSeatArea eventSeatArea = getEventSeatArea(areaId);
-		eventSeatAreaRepository.delete(eventSeatArea);
-	}
-
-	public void updateEventSeatArea(Long areaId, EventSeatAreaUpdateRequest request) {
-		EventSeatArea eventSeatArea = getEventSeatArea(areaId);
-		eventSeatArea.updateEventSeatAreaPriceAndType(request.seatAreaType(), request.price());
-	}
-
-	@Transactional(readOnly = true)
-	public List<EventSeatAreaResponse> getEventSeatAreas(Long id) {
-		Event event = getEvent(id);
-		List<EventSeatArea> eventSeatAreas = eventSeatAreaRepository.findEventSeatAreasByEvent(event);
-
-		return eventSeatAreas.stream()
-			.map(EventSeatAreaResponse::of)
-			.toList();
-	}
-
 	private Event getEvent(Long eventId) {
 		return eventRepository.findById(eventId)
 			.orElseThrow(() -> new CustomException(EVENT_NOT_FOUND));
@@ -133,10 +115,5 @@ public class EventService {
 	private EventHall getEventHall(Long eventHallId) {
 		return eventHallRepository.findById(eventHallId)
 			.orElseThrow(() -> new CustomException(EVENT_HALL_NOT_FOUND));
-	}
-
-	private EventSeatArea getEventSeatArea(Long areaId) {
-		return eventSeatAreaRepository.findById(areaId)
-			.orElseThrow(EventSeatAreaNotFoundException::new);
 	}
 }
