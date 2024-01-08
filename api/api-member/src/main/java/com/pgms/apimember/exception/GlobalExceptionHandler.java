34b9397cd3b3f40036dbc0267b8afec1ab4b61f7
ebcho <joeunbi514@naver.com>
2024-01-08 15:46:30 +0900

    refactor: GlobalExceptionHandler 리팩터링

--- api/api-member/src/main/java/com/pgms/apimember/exception/GlobalExceptionHandler.java
+++ api/api-member/src/main/java/com/pgms/apimember/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.pgms.apimember.exception;
 
+import static com.pgms.coredomain.domain.common.GlobalErrorCode.*;
+
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.pgms.coredomain.domain.common.BaseErrorCode;
-import com.pgms.coredomain.domain.common.MemberErrorCode;
 import com.pgms.coredomain.response.ErrorResponse;
+import com.pgms.coresecurity.security.exception.SecurityCustomException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,13 +25,19 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
 		log.error(">>>>> Internal Server Error : {}", ex);
-		ErrorResponse errorResponse = new ErrorResponse("INTERNAL SERVER ERROR", ex.getMessage());
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(INTERNAL_SERVER_ERROR.getErrorResponse());
 	}
 
 	@ExceptionHandler(CustomException.class)
-	protected ResponseEntity<ErrorResponse> handleEventCustomException(CustomException ex) {
-		log.warn(">>>>> Custom Exception : {}", ex);
+	protected ResponseEntity<ErrorResponse> handleMemberCustomException(CustomException ex) {
+		log.warn(">>>>> MemberCustomException : {}", ex);
+		BaseErrorCode errorCode = ex.getErrorCode();
+		return ResponseEntity.status(errorCode.getStatus()).body(errorCode.getErrorResponse());
+	}
+
+	@ExceptionHandler(SecurityCustomException.class)
+	protected ResponseEntity<ErrorResponse> handleSecurityCustomException(SecurityCustomException ex) {
+		log.warn(">>>>> SecurityCustomException : {}", ex);
 		BaseErrorCode errorCode = ex.getErrorCode();
 		return ResponseEntity.status(errorCode.getStatus()).body(errorCode.getErrorResponse());
 	}
@@ -40,7 +48,7 @@ public class GlobalExceptionHandler {
 		BindingResult bindingResult = ex.getBindingResult();
 
 		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-		ErrorResponse errorResponse = MemberErrorCode.VALIDATION_FAILED.getErrorResponse();
+		ErrorResponse errorResponse = VALIDATION_FAILED.getErrorResponse();
 		fieldErrors.forEach(error -> errorResponse.addValidation(error.getField(), error.getDefaultMessage()));
 		return ResponseEntity.status(ex.getStatusCode()).body(errorResponse);
 	}
