71b71a8e2401b0c21c946dff38b4c6fee71774dd
park0jae <okvv26@naver.com>
2024-01-09 11:59:46 +0900

    refactor: #169 계층간 DTO 분리를 위해 검색관련 DTO 객체 추가 처리

---
+++ core/core-infra-es/src/main/java/com/pgms/coreinfraes/dto/EventDocumentResponse.java
@@ -0,0 +1,29 @@
+package com.pgms.coreinfraes.dto;
+
+import com.pgms.coreinfraes.document.EventDocument;
+
+public record EventDocumentResponse(
+	Long id,
+	String title,
+	String description,
+	String startedAt,
+	String endedAt,
+	String viewRating,
+	String genreType,
+	Double average,
+	Long eventHallId
+) {
+	public static EventDocumentResponse of(EventDocument eventDocument) {
+		return new EventDocumentResponse(
+			eventDocument.getId(),
+			eventDocument.getTitle(),
+			eventDocument.getDescription(),
+			eventDocument.getStartedAt().toString(),
+			eventDocument.getEndedAt().toString(),
+			eventDocument.getViewRating(),
+			eventDocument.getGenreType().toString(),
+			eventDocument.getAverageScore(),
+			eventDocument.getEventHallId()
+		);
+	}
+}
