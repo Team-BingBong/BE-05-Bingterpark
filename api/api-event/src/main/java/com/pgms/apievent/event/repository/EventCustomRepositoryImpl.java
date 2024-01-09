002b3c079605939fe9f9abb9f798858df65e533d
park0jae <okvv26@naver.com>
2024-01-09 15:59:34 +0900

    fix: #175 공연 목록 조회 - 랭킹순 쿼리 수정

--- api/api-event/src/main/java/com/pgms/apievent/event/repository/EventCustomRepositoryImpl.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/repository/EventCustomRepositoryImpl.java
@@ -39,11 +39,11 @@ public class EventCustomRepositoryImpl implements EventCustomRepository {
 			.with(LocalTime.MIN);
 
 		List<EventResponse> content = jpaQueryFactory.selectFrom(event)
-			.join(eventTime)
+			.leftJoin(eventTime)
 			.on(eventTime.event.eq(event))
 			.leftJoin(booking)
 			.on(booking.time.eq(eventTime))
-			.where(event.genreType.eq(genre), booking.createdAt.goe(cmpDate))
+			.where(event.genreType.eq(genre), booking.createdAt.coalesce(cmpDate).goe(cmpDate))
 			.groupBy(event.id)
 			.orderBy(booking.count().desc())
 			.offset(offset)
