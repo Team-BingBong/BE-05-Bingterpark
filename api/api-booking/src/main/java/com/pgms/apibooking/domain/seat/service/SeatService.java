e65ae678fe645e228ad1215b32ff6761f0dc9a00
Hanna <hanna@trdst.com>
2024-01-10 19:14:07 +0900

    refactor: getSeats 리팩터링

--- api/api-booking/src/main/java/com/pgms/apibooking/domain/seat/service/SeatService.java
+++ api/api-booking/src/main/java/com/pgms/apibooking/domain/seat/service/SeatService.java
@@ -1,17 +1,16 @@
 package com.pgms.apibooking.domain.seat.service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.pgms.apibooking.common.exception.BookingException;
 import com.pgms.apibooking.domain.seat.dto.request.SeatsGetRequest;
 import com.pgms.apibooking.domain.seat.dto.response.AreaResponse;
 import com.pgms.coredomain.domain.common.BookingErrorCode;
-import com.pgms.apibooking.common.exception.BookingException;
 import com.pgms.coredomain.domain.event.EventSeat;
 import com.pgms.coredomain.domain.event.EventSeatStatus;
 import com.pgms.coredomain.domain.event.repository.EventSeatRepository;
@@ -28,12 +27,9 @@ public class SeatService { //TODO: 테스트 코드 작성
 
 	@Transactional(readOnly = true)
 	public List<AreaResponse> getSeats(SeatsGetRequest request) {
-		List<EventSeat> seats = eventSeatRepository.findAllWithAreaByTimeId(request.eventTimeId());
-
-		Map<Long, List<EventSeat>> seatsByArea = seats.stream()
-			.collect(Collectors.groupingBy(es -> es.getEventSeatArea().getId()));
-
-		return seatsByArea.values().stream()
+		return eventSeatRepository.findAllWithAreaByTimeId(request.eventTimeId()).stream()
+			.collect(Collectors.groupingBy(es -> es.getEventSeatArea().getId()))
+			.values().stream()
 			.map(AreaResponse::from)
 			.toList();
 	}
