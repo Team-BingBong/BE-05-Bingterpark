6af23ddd2819205fae65a508964683d17a8c7b09
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-01-10 14:17:36 +0900

    feat: 검색 엔그램 적용 (#199)
    * fix: 공연장 좌석 읽어오지 못하는 오류 수정, 공연장 좌석-공연장 연관관계 설정 메소드 추가
    
    * fix: #189 공연장 좌석 없을 경우 null 체크 조건 추가
    
    * feat: #194 엔그램 토크나이저 적용, 모델어트리뷰트 에러 핸들링 추가

--- api/api-event/src/main/java/com/pgms/apievent/eventSearch/controller/EventSearchController.java
+++ api/api-event/src/main/java/com/pgms/apievent/eventSearch/controller/EventSearchController.java
@@ -4,16 +4,20 @@ import com.pgms.apievent.common.dto.response.PageResponseDto;
 import com.pgms.apievent.eventSearch.dto.request.EventKeywordSearchRequest;
 import com.pgms.apievent.eventSearch.dto.response.RecentTop10KeywordsResponse;
 import com.pgms.apievent.eventSearch.service.EventSearchService;
+import com.pgms.apievent.exception.EventException;
 import com.pgms.coredomain.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static com.pgms.apievent.exception.EventErrorCode.BINDING_FAILED_EXCEPTION;
 
 @RestController
 @RequestMapping("/api/v1/events/search")
@@ -24,7 +28,12 @@ public class EventSearchController {
 
 	@GetMapping("/keyword")
 	public ResponseEntity<ApiResponse> searchEventsByKeyword(
-		@ModelAttribute @Valid EventKeywordSearchRequest eventKeywordSearchRequest) {
+		@ModelAttribute @Valid EventKeywordSearchRequest eventKeywordSearchRequest,
+		BindingResult bindingResult) {
+		if(bindingResult.hasErrors()){
+			throw new EventException(BINDING_FAILED_EXCEPTION);
+		}
+
 		PageResponseDto response = eventSearchService.searchEventsByKeyword(eventKeywordSearchRequest);
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
