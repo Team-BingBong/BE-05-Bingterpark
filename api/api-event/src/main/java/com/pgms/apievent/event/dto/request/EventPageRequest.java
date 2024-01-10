eef94536605bc44e84cd96dda1d19b6e56b03070
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-01-10 10:33:40 +0900

    refactor: 공연 도메인 validation 추가 (#186)
    * refactor: #183 dto validation 전체적으로 다듬기
    
    * feat: #183 날짜 직렬화 양식 및 validation 추가

--- api/api-event/src/main/java/com/pgms/apievent/event/dto/request/EventPageRequest.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/dto/request/EventPageRequest.java
@@ -1,12 +1,14 @@
 package com.pgms.apievent.event.dto.request;
 
 import com.pgms.apievent.common.dto.request.PageRequestDto;
-
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 
 @Getter
 public class EventPageRequest extends PageRequestDto {
 	private String genreType;
+	@Min(1) @Max(10000)
 	private Integer dateOffset;
 
 	public EventPageRequest(Integer page, Integer size, String genreType, Integer dateOffset) {
