ede5974b42b30e195a05738658c318a529efa073
park0jae <okvv26@naver.com>
2024-01-11 11:45:44 +0900

    feat: #219 EventController Swagger 문서화 어노테이션 추가

--- api/api-event/src/main/java/com/pgms/apievent/event/controller/EventController.java
+++ api/api-event/src/main/java/com/pgms/apievent/event/controller/EventController.java
@@ -22,9 +22,12 @@ import com.pgms.apievent.event.dto.response.EventResponse;
 import com.pgms.apievent.event.service.EventService;
 import com.pgms.coredomain.response.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "공연", description = "공연 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/events")
@@ -32,6 +35,7 @@ public class EventController {
 
 	private final EventService eventService;
 
+	@Operation(summary = "공연 생성", description = "공연 생성 메서드입니다.")
 	@PostMapping
 	public ResponseEntity<ApiResponse> createEvent(@Valid @RequestBody EventCreateRequest request) {
 		EventResponse response = eventService.createEvent(request);
@@ -42,12 +46,14 @@ public class EventController {
 		return ResponseEntity.created(location).body(ApiResponse.created(response));
 	}
 
+	@Operation(summary = "공연ID 조회", description = "공연을 ID로 조회하는 메서드입니다.")
 	@GetMapping("/{id}")
 	public ResponseEntity<ApiResponse> getEventById(@PathVariable Long id) {
 		EventResponse response = eventService.getEventById(id);
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "공연 목록 조회 - 랭킹순", description = "공연 목록을 예매가 많은 순서로 조회하는 메서드입니다.")
 	@GetMapping("/sort/ranking")
 	public ResponseEntity<ApiResponse> getEventsPageByGenreSortedByRanking(
 		@Valid @ModelAttribute EventPageRequest request) {
@@ -55,6 +61,7 @@ public class EventController {
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "공연 목록 조회 - 리뷰순", description = "공연 목록을 후기가 많은 순서로 조회하는 메서드입니다.")
 	@GetMapping("/sort/review")
 	public ResponseEntity<ApiResponse> getEventsPageByGenreSortedByReview(
 		@Valid @ModelAttribute EventPageRequest request) {
@@ -62,6 +69,7 @@ public class EventController {
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "공연 목록 조회 - 마감임박순", description = "공연 목록을 예매 마감일 임박 순서로 조회하는 메서드입니다.")
 	@GetMapping("/sort/ended-at")
 	public ResponseEntity<ApiResponse> getEventsPageByGenreSortedByBookingEndedAt(
 		@Valid @ModelAttribute EventPageRequest request) {
@@ -69,6 +77,7 @@ public class EventController {
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "공연 정보 수정", description = "공연 정보를 수정하는 메서드입니다.")
 	@PutMapping("/{id}")
 	public ResponseEntity<ApiResponse> updateEvent(
 		@PathVariable Long id,
@@ -77,6 +86,7 @@ public class EventController {
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "공연 삭제", description = "공연을 ID로 삭제하는 메서드입니다.")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteEventById(@PathVariable Long id) {
 		eventService.deleteEventById(id);
