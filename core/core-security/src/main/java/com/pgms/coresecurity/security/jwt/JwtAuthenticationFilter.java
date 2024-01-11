5cfcbff60c8c8cf16edf1775c97899d60e414fd4
조은비 <69096886+eunbc@users.noreply.github.com>
2024-01-11 14:06:39 +0900

    fix: WebSecurityConfig 수정, 커스텀 필터 로직 수정 (#222)
    * fix: 커스텀 필터 로직 수정
    
    * fix: WebSecurityConfig 수정

--- core/core-security/src/main/java/com/pgms/coresecurity/security/jwt/JwtAuthenticationFilter.java
+++ core/core-security/src/main/java/com/pgms/coresecurity/security/jwt/JwtAuthenticationFilter.java
@@ -2,6 +2,9 @@ package com.pgms.coresecurity.security.jwt;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -14,7 +17,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * JwtAuthenticationFilter는
+ * jwt 토큰의 존재여부, 유효기간, 형식 검증 후 SecurityContextHolder에 setAuthentication을 해주는 역할입니다.
+ */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -25,15 +34,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		throws ServletException, IOException {
 		try {
 			String accessToken = parseJwt(request);
-			if (accessToken != null && jwtTokenProvider.validateAccessToken(accessToken)) {
-				Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-			}
+			jwtTokenProvider.validateAccessToken(accessToken);
+			Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
 		} catch (ExpiredJwtException e) {
-			logger.warn("JWT token is expired: ", e);
-			request.setAttribute("expired", e.getMessage());
+			logger.warn("ExpiredJwtException Occurred : ", e);
+			throw new CredentialsExpiredException("토큰의 유효기간이 만료되었습니다.", e);
 		} catch (Exception e) {
-			logger.error("Cannot set user authentication: ", e);
+			logger.warn("JwtAuthentication Failed. : ", e);
+			throw new BadCredentialsException("토큰 인증에 실패하였습니다.");
 		}
 
 		filterChain.doFilter(request, response);
@@ -46,6 +55,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			return headerAuth.substring(7);
 		}
 
-		return null;
+		throw new AuthenticationCredentialsNotFoundException("토큰이 존재하지 않습니다.");
 	}
 }
