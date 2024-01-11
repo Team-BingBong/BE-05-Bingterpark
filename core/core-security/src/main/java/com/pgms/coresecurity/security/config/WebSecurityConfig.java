76addaa7eade0761ebb2b404b29196b1f41c2619
조은비 <69096886+eunbc@users.noreply.github.com>
2024-01-11 18:55:57 +0900

    feat: 전체 엔드포인트 권한 설정 (#225)
    * fix: filter 중복 등록 방지
    
    * refactor: httpSecurity 공통 설정 분리
    
    * feat: booking 모듈 엔드포인트 권한 추가
    
    * feat: 공연 엔드포인트 권한 설정
    
    * chore: 공백 수정
    
    * chore: .http 파일별 authorization 헤더 추가
    
    * fix: default를 authenticated 로 변경

--- core/core-security/src/main/java/com/pgms/coresecurity/security/config/WebSecurityConfig.java
+++ core/core-security/src/main/java/com/pgms/coresecurity/security/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.pgms.coresecurity.security.config;
 
+import static com.pgms.coredomain.domain.member.enums.Role.*;
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.*;
 
 import java.util.List;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,7 +21,6 @@ import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import com.pgms.coredomain.domain.member.enums.Role;
 import com.pgms.coresecurity.security.jwt.JwtAccessDeniedHandler;
 import com.pgms.coresecurity.security.jwt.JwtAuthenticationEntryPoint;
 import com.pgms.coresecurity.security.jwt.JwtAuthenticationFilter;
@@ -52,27 +54,43 @@ public class WebSecurityConfig {
 	 */
 	@Bean
 	public SecurityFilterChain securityFilterChainPermitAll(HttpSecurity http) throws Exception {
+		configureCommonSecuritySettings(http);
 		http
 			.securityMatchers(matchers -> matchers
 				.requestMatchers(requestPermitAll())
 			)
-			.authorizeHttpRequests().anyRequest().permitAll().and()
-
-			// filter 비활성화
-			.csrf().disable()
-			.anonymous().disable()
-			.formLogin().disable()
-			.httpBasic().disable()
-			.rememberMe().disable()
-			.logout().disable()
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+			.authorizeHttpRequests().anyRequest().permitAll();
 		return http.build();
 	}
 
 	private RequestMatcher[] requestPermitAll() {
 		List<RequestMatcher> requestMatchers = List.of(
+			// MEMBER
 			antMatcher("/api/*/auth/**"),
 			antMatcher("/api/*/members/signup"),
+
+			// BOOKING
+			antMatcher(GET, "/api/*/payments/**"),
+			antMatcher(POST, "/api/*/payments/**"),
+			antMatcher("/bookings"),
+			antMatcher("/payments"),
+
+			// EVENT
+			antMatcher(GET, "/api/*/events/*"),               // 공연 아이디 조회
+			antMatcher(GET, "/api/*/events/sort/ranking"),    // 공연 목록 조회 - 랭킹순
+			antMatcher(GET, "/api/*/events/sort/review"),     // 공연 목록 조회 - 리뷰순
+			antMatcher(GET, "/api/*/events/sort/ended-at"),   // 공연 목록 조회 - 예약마감일자순
+			antMatcher(GET, "/api/*/events/search/keyword"),  // 공연 키워드 검색
+			antMatcher(GET, "/api/*/events/search/top-ten"),  // 실시간 검색어 순위
+			antMatcher(GET, "/api/*/event-times/*"),             // 회차 아이디로 회차 정보 조회
+			antMatcher(GET, "/api/*/event-times/events/*"),   // 공연 아이디로 특정 공연에 대한 전체 회차 정보 조회
+			antMatcher(GET, "/api/*/event-halls/*"),             // 공연장 아이디 조회
+			antMatcher(GET, "/api/*/event-halls"),             // 공연장 목록 조회
+			antMatcher(GET, "/api/*/event-seats/event-times/*/seats"),    // 회차별 공연 좌석 조회
+			antMatcher(GET, "/api/*/event-seats/event-times/*/available-numbers"),    // 회차별 공연 좌석 구역별 남은 자리 수 조회
+			antMatcher(GET, "/api/*/events/*/seat-area"),    // 공연 좌석 구역 목록 조회
+
+			// DOCS
 			antMatcher("/v3/api-docs/**"),
 			antMatcher("/swagger-ui/**")
 		);
@@ -84,6 +102,7 @@ public class WebSecurityConfig {
 	 */
 	@Bean
 	public SecurityFilterChain securityFilterChainOAuth(HttpSecurity http) throws Exception {
+		configureCommonSecuritySettings(http);
 		http
 			.securityMatchers(matchers -> matchers
 				.requestMatchers(
@@ -97,16 +116,7 @@ public class WebSecurityConfig {
 				.loginPage("/login")
 				.successHandler(oauthSuccessHandler)
 				.userInfoEndpoint()
-				.userService(oAuth2UserService))
-
-			// 필터 비활성화
-			.csrf().disable()
-			.anonymous().disable()
-			.formLogin().disable()
-			.httpBasic().disable()
-			.rememberMe().disable()
-			.logout().disable()
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				.userService(oAuth2UserService));
 		return http.build();
 	}
 
@@ -115,6 +125,7 @@ public class WebSecurityConfig {
 	 */
 	@Bean
 	public SecurityFilterChain securityFilterChainAuthorized(HttpSecurity http) throws Exception {
+		configureCommonSecuritySettings(http);
 		http
 			.securityMatchers(matchers -> matchers
 				.requestMatchers(requestHasRoleUser())
@@ -122,25 +133,22 @@ public class WebSecurityConfig {
 				.requestMatchers(requestHasRoleSuperAdmin())
 			)
 			.authorizeHttpRequests(auth -> auth
-				// TODO 엔드포인트별 authorize 통합
-				.requestMatchers(requestHasRoleSuperAdmin()).hasAuthority(Role.ROLE_SUPERADMIN.name())
-				.requestMatchers(requestHasRoleAdmin()).hasAuthority(Role.ROLE_ADMIN.name())
-				.requestMatchers(requestHasRoleUser()).hasAuthority(Role.ROLE_USER.name()))
+				.requestMatchers(requestHasRoleSuperAdmin()).hasAuthority(ROLE_SUPERADMIN.name())
+				.requestMatchers(requestHasRoleAdmin()).hasAuthority(ROLE_ADMIN.name())
+				.requestMatchers(requestHasRoleUser()).hasAuthority(ROLE_USER.name()))
 			.exceptionHandling(exception -> {
 				exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
 				exception.accessDeniedHandler(jwtAccessDeniedHandler);
 			})
-			.addFilterAfter(jwtAuthenticationFilter, ExceptionTranslationFilter.class)
-
-			// filter 비활성화
-			.csrf().disable()
-			.anonymous().disable()
-			.formLogin().disable()
-			.httpBasic().disable()
-			.rememberMe().disable()
-			.logout().disable()
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+			.addFilterAfter(jwtAuthenticationFilter, ExceptionTranslationFilter.class);
 		return http.build();
+	}
+
+	@Bean
+	public FilterRegistrationBean<JwtAuthenticationFilter> filterRegistration(JwtAuthenticationFilter filter) {
+		FilterRegistrationBean<JwtAuthenticationFilter> registration = new FilterRegistrationBean<>(filter);
+		registration.setEnabled(false);
+		return registration;
 	}
 
 	private RequestMatcher[] requestHasRoleSuperAdmin() {
@@ -151,37 +159,81 @@ public class WebSecurityConfig {
 
 	private RequestMatcher[] requestHasRoleAdmin() {
 		List<RequestMatcher> requestMatchers = List.of(
-			antMatcher("/api/*/admin/**"));
+			// MEMBER
+			antMatcher("/api/*/admin/**"),
+
+			// EVENT
+			antMatcher(POST, "/api/*/events"),                // 공연 생성
+			antMatcher(PUT, "/api/*/events/*"),               // 공연 수정
+			antMatcher(DELETE, "/api/*/events/*"),            // 공연 삭제
+			antMatcher(POST, "/api/*/event-times/*"),         // 공연 회차 생성
+			antMatcher(PATCH, "/api/*/event-times/*"),         // 회차 아이디로 회차 정보 수정
+			antMatcher(DELETE, "/api/*/event-times/*"),        // 회차 아이디로 회차 삭제
+			antMatcher(POST, "/api/*/event-halls"),            // 공연장 생성
+			antMatcher(DELETE, "/api/*/event-halls/*"),        // 공연장 삭제
+			antMatcher(PUT, "/api/*/event-halls/*"),           // 공연장 수정
+			antMatcher(POST, "/api/*/event-seats/events/*"),   // 공연 좌석 생성
+			antMatcher(PATCH, "/api/*/event-seats/seat-area"),   // 공연 좌석 등급 일괄 수정
+			antMatcher(DELETE, "/api/*/event-seats"),            // 공연 좌석 일괄 삭제
+			antMatcher(POST, "/api/*/events/*/seat-area"),       // 공연 좌석 구역 생성
+			antMatcher(DELETE, "/api/*/events/seat-area/*"),     // 공연 좌석 구역 삭제
+			antMatcher(PUT, "/api/*/events/seat-area/*"),        // 공연 좌석 구역 수정
+			antMatcher(POST, "/api/*/thumbnails/events/*"),      // 공연 썸네일 이미지 생성
+			antMatcher(PATCH, "/api/*/thumbnails/events/*"),     // 공연 썸네일 이미지 수정
+			antMatcher(POST, "/api/*/event-images/events/*"),    // 공연 상세 이미지 추가
+			antMatcher(DELETE, "/api/*/event-images/events/*")   // 공연 상세 이미지 삭제
+		);
 		return requestMatchers.toArray(RequestMatcher[]::new);
 	}
 
 	private RequestMatcher[] requestHasRoleUser() {
 		List<RequestMatcher> requestMatchers = List.of(
-			antMatcher("/api/*/members/**"));
+			// MEMBER
+			antMatcher("/api/*/members/**"),
+
+			// BOOKING
+			antMatcher(GET, "/api/*/bookings/**"),
+			antMatcher(POST, "/api/*/bookings/**"),
+			antMatcher(GET, "/api/*/seats/**"),
+			antMatcher(POST, "/api/*/seats/**"),
+
+			// EVENT
+			antMatcher(POST, "/api/*/event-reviews/*"), // 공연 후기 생성
+			antMatcher(PATCH, "/api/*/event-reviews/*"), // 공연 후기 수정
+			antMatcher(GET, "/api/*/event-reviews/*"), // 리뷰 아이디로 조회
+			antMatcher(GET, "/api/*/event-reviews/events/*"), // 특정 공연에 대한 리뷰 전체 조회
+			antMatcher(DELETE, "/api/*/event-reviews/*") // 리뷰 아이디로 삭제
+		);
 		return requestMatchers.toArray(RequestMatcher[]::new);
 	}
 
 	/**
-	 * 위에서 정의된 엔드포인트 이외에는 denyAll 로 설정
+	 * 위에서 정의된 엔드포인트 이외에는 authenticated 로 설정
 	 */
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChainDefault(HttpSecurity http) throws Exception {
+		configureCommonSecuritySettings(http);
 		http
-			.authorizeHttpRequests().anyRequest().permitAll().and() // TODO 완성 후 denyAll 로 변경, exceptionHandling 삭제하기
+			.authorizeHttpRequests()
+			.anyRequest().authenticated()
+			.and()
+			.addFilterAfter(jwtAuthenticationFilter, ExceptionTranslationFilter.class)
 			.exceptionHandling(exception -> {
 				exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
 				exception.accessDeniedHandler(jwtAccessDeniedHandler);
-			})
+			});
+		return http.build();
+	}
 
-			// filter 비활성화
+	private void configureCommonSecuritySettings(HttpSecurity http) throws Exception {
+		http
 			.csrf().disable()
 			.anonymous().disable()
 			.formLogin().disable()
 			.httpBasic().disable()
 			.rememberMe().disable()
 			.logout().disable()
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		return http.build();
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-
 }
