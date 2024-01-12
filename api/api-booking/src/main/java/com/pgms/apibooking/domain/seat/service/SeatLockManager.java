7f318d93b29a700575c3d89bc1acf729a10ad016
Hanna <hanna@trdst.com>
2024-01-12 20:27:05 +0900

    refactor: SeatLockManager RedisOperator 적용

--- api/api-booking/src/main/java/com/pgms/apibooking/domain/seat/service/SeatLockManager.java
+++ api/api-booking/src/main/java/com/pgms/apibooking/domain/seat/service/SeatLockManager.java
@@ -1,10 +1,10 @@
 package com.pgms.apibooking.domain.seat.service;
 
-import java.time.Duration;
 import java.util.Optional;
 
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import com.pgms.apibooking.common.util.RedisOperator;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,23 +14,23 @@ public class SeatLockManager {
 
 	private final static String SEAT_LOCK_CACHE_KEY_PREFIX = "seatId:";
 	private final static String SEAT_LOCK_CACHE_VALUE_PREFIX = "memberId:";
-	private final RedisTemplate<String, String> redisTemplate;
+
+	private final RedisOperator redisOperator;
 
 	public Optional<Long> getSelectorId(Long seatId) {
 		String key = generateSeatLockKey(seatId);
-		String value = redisTemplate.opsForValue().get(key);
+		String value = redisOperator.get(key);
 		return Optional.ofNullable(value == null ? null : extractMemberId(value));
 	}
 
 	public void lockSeat(Long seatId, Long memberId, Integer expirationSeconds) {
 		String key = generateSeatLockKey(seatId);
 		String value = generateSeatLockValue(memberId);
-		Duration timeout = Duration.ofSeconds(expirationSeconds);
-		redisTemplate.opsForValue().setIfAbsent(key, value, timeout);
+		redisOperator.setIfAbsent(key, value, expirationSeconds);
 	}
 
 	public void unlockSeat(Long seatId) {
-		redisTemplate.delete(generateSeatLockKey(seatId));
+		redisOperator.delete(generateSeatLockKey(seatId));
 	}
 
 	private String generateSeatLockKey(Long seatId) {
