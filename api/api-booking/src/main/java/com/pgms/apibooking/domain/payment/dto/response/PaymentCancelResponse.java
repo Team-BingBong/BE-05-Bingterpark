e8ccb97c946a107bcb1698e67f5f209953cecdee
byulcode <bybit0408@gmail.com>
2024-01-12 22:55:19 +0900

    test: 결제 테스트코드 작성

--- api/api-booking/src/main/java/com/pgms/apibooking/domain/payment/dto/response/PaymentCancelResponse.java
+++ api/api-booking/src/main/java/com/pgms/apibooking/domain/payment/dto/response/PaymentCancelResponse.java
@@ -5,7 +5,7 @@ import java.util.List;
 public record PaymentCancelResponse(
 	String paymentKey,
 	String orderId,
-	String orderName,	// 토스페이먼츠에서 제공해주는 값이라 booing으로 변경x
+	String orderName,	// 토스페이먼츠에서 제공해주는 값이라 booking으로 변경x
 	String method,
 	int totalAmount,
 	String status,
--- api/api-booking/src/test/java/com/pgms/apibooking/service/PaymentServiceTest.java
+++ api/api-booking/src/test/java/com/pgms/apibooking/service/PaymentServiceTest.java
@@ -3,6 +3,7 @@ package com.pgms.apibooking.service;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.pgms.apibooking.common.exception.BookingException;
 import com.pgms.apibooking.config.TestConfig;
+import com.pgms.apibooking.domain.payment.dto.request.PaymentCancelRequest;
 import com.pgms.apibooking.domain.payment.dto.response.PaymentSuccessResponse;
 import com.pgms.apibooking.domain.payment.service.PaymentService;
 import com.pgms.apibooking.factory.BookingFactory;
@@ -22,14 +25,17 @@ import com.pgms.apibooking.factory.EventSeatFactory;
 import com.pgms.apibooking.factory.EventTimeFactory;
 import com.pgms.apibooking.factory.PaymentFactory;
 import com.pgms.apibooking.factory.TicketFactory;
+import com.pgms.coredomain.domain.booking.BankCode;
 import com.pgms.coredomain.domain.booking.Booking;
 import com.pgms.coredomain.domain.booking.BookingStatus;
+import com.pgms.coredomain.domain.booking.CardIssuer;
 import com.pgms.coredomain.domain.booking.Payment;
 import com.pgms.coredomain.domain.booking.PaymentMethod;
 import com.pgms.coredomain.domain.booking.PaymentStatus;
 import com.pgms.coredomain.domain.booking.Ticket;
 import com.pgms.coredomain.domain.booking.repository.BookingRepository;
 import com.pgms.coredomain.domain.booking.repository.PaymentRepository;
+import com.pgms.coredomain.domain.common.BookingErrorCode;
 import com.pgms.coredomain.domain.event.Event;
 import com.pgms.coredomain.domain.event.EventHall;
 import com.pgms.coredomain.domain.event.EventSeat;
@@ -70,8 +76,10 @@ class PaymentServiceTest {
 
 	@Autowired
 	private PaymentRepository paymentRepository;
+
 	@Autowired
 	private EventSeatRepository eventSeatRepository;
+
 	@Autowired
 	private MemberRepository memberRepository;
 
@@ -79,7 +87,6 @@ class PaymentServiceTest {
 	private PaymentService paymentService;
 
 	private Member member;
-	private Booking booking;
 	private static final String PAYMENT_KEY = "paymentTestKey";
 
 	@BeforeEach
@@ -94,7 +101,7 @@ class PaymentServiceTest {
 	}
 
 	@Test
-	public void 예매_카드_결제를_진행한다() {
+	public void 카드_결제를_진행한다() {
 		// given
 		LocalDateTime eventStartedAt = NOW.plusDays(2);
 		LocalDateTime eventEndedAt = NOW.plusDays(2).plusMinutes(120);
@@ -138,79 +145,201 @@ class PaymentServiceTest {
 		booking.updatePayment(payment);
 		bookingRepository.save(booking);
 
-		//when
+		// when
 		PaymentSuccessResponse response = paymentService.successPayment(PAYMENT_KEY, booking.getId(),
-			payment.getAmount());
+			booking.getAmount());
 
-		//then
+		// then
 		payment = paymentRepository.findByPaymentKey(PAYMENT_KEY).get();
 		booking = bookingRepository.findWithPaymentById(booking.getId()).get();
 
-		assertThat(response.status()).isEqualTo(PaymentStatus.DONE.toString());
-		assertThat(response.card().number()).isEqualTo(payment.getCardNumber());
-
+		assertThat(payment.getStatus()).isEqualTo(PaymentStatus.DONE);
 		assertThat(booking.getStatus()).isEqualTo(BookingStatus.PAYMENT_COMPLETED);
+
+		assertThat(response.paymentKey()).isEqualTo(payment.getPaymentKey());
+		assertThat(response.card().number()).isEqualTo(payment.getCardNumber());
+		assertThat(CardIssuer.fromOfficialCode(response.card().issuerCode())).isEqualTo(payment.getCardIssuer());
 		assertThat(payment.getAccountNumber()).isNull();
 	}
 
-	// @Test
-	// public void 예매가격_결제가격_불일치시_결제를_진행할_수_없다() {
-	// 	// given
-	// 	String paymentKey = "paymentKey";
-	// 	Payment payment = Payment.builder()
-	// 		.method(PaymentMethod.CARD)
-	// 		.amount(booking.getAmount())
-	// 		.status(PaymentStatus.READY)
-	// 		.build();
-	// 	booking.updatePayment(payment);
-	// 	when(bookingRepository.findWithPaymentById(anyString())).thenReturn(Optional.of(booking));
-	//
-	// 	// when & then
-	// 	assertThatThrownBy(() -> paymentService.successPayment(paymentKey, booking.getId(), 170000))
-	// 		.isInstanceOf(BookingException.class)
-	// 		.hasMessageContaining(BookingErrorCode.PAYMENT_AMOUNT_MISMATCH.getMessage());
-	// }
-	//
-	// @Test
-	// public void 카드_결제_취소_테스트() {
-	// 	String paymentKey = "paymentKey";
-	// 	Payment payment = Payment.builder()
-	// 		.method(PaymentMethod.CARD)
-	// 		.amount(booking.getAmount())
-	// 		.status(PaymentStatus.READY)
-	// 		.build();
-	// 	booking.updatePayment(payment);
-	//
-	// 	BookingCancelRequest cancelRequest = new BookingCancelRequest(
-	// 		"고객이 결제 취소",
-	// 		180000,
-	// 		Optional.of(new RefundAccountRequest("20", "352-123", "김환불"))
-	// 	);
-	//
-	// 	PaymentCancelResponse mockCancelResponse = new PaymentCancelResponse(
-	// 		paymentKey,
-	// 		booking.getId(),
-	// 		booking.getBookingName(),
-	// 		payment.getMethod().getDescription(),
-	// 		"180000",
-	// 		"CANCELED",
-	// 		"2024-01-01T10:01:00+05:00",
-	// 		"2024-01-01T10:01:00+05:00",
-	// 		new PaymentCardResponse("card_num", 2, false),
-	// 		null,
-	// 		List.of(new PaymentCancelDetailResponse(cancelRequest.cancelReason(), 180000, "2024-01-02T10:01:00+05:00"))
-	// 	);
-	//
-	// 	when(paymentRepository.findByPaymentKey(paymentKey)).thenReturn(Optional.of(payment));
-	// 	when(restTemplate.postForObject(any(), any(HttpEntity.class), eq(PaymentCancelResponse.class)))
-	// 		.thenReturn(mockCancelResponse);
-	//
-	// 	// when
-	// 	PaymentCancelResponse response = paymentService.cancelPayment(paymentKey, cancelRequest);
-	//
-	// 	// then
-	// 	verify(restTemplate, times(1)).postForObject(any(), any(HttpEntity.class), eq(PaymentCancelResponse.class));
-	// 	assertThat(payment.getStatus()).isEqualTo(PaymentStatus.CANCELED);
-	// 	assertThat(booking.getStatus()).isEqualTo(BookingStatus.CANCELED);
-	// }
+	@Test
+	public void 예매가격_결제가격_불일치시_결제를_진행할_수_없다() {
+		// given
+		LocalDateTime eventStartedAt = NOW.plusDays(2);
+		LocalDateTime eventEndedAt = NOW.plusDays(2).plusMinutes(120);
+		LocalDateTime bookingStartedAt = NOW;
+		LocalDateTime paymentRequestedAt = NOW.plusMinutes(2);
+		LocalDateTime bookingEndedAt = NOW.plusDays(1);
+
+		EventHall hall = EventHallFactory.generate();
+		eventHallRepository.save(hall);
+
+		Event event = EventFactory.generate(
+			hall,
+			eventStartedAt,
+			eventEndedAt,
+			bookingStartedAt,
+			bookingEndedAt
+		);
+		eventRepository.save(event);
+
+		EventTime time = EventTimeFactory.generate(event, eventStartedAt, eventEndedAt);
+		eventTimeRepository.save(time);
+
+		EventSeatArea area = EventSeatAreaFactory.generate(event, SeatAreaType.S);
+		eventSeatAreaRepository.save(area);
+
+		EventSeat seat = EventSeatFactory.generate(time, area, "A1", EventSeatStatus.AVAILABLE);
+		eventSeatRepository.save(seat);
+
+		Booking booking = BookingFactory.generate(
+			member,
+			time,
+			seat.getEventSeatArea().getPrice(),
+			BookingStatus.WAITING_FOR_PAYMENT
+		);
+		Ticket ticket = TicketFactory.generate(seat);
+		booking.addTicket(ticket);
+
+		int actualAmount = booking.getAmount();
+		int notSameAmount = 1000;
+
+		Payment payment = PaymentFactory.generate(
+			PaymentMethod.CARD,
+			notSameAmount,
+			PaymentStatus.READY
+		);
+		booking.updatePayment(payment);
+		bookingRepository.save(booking);
+
+		// when & then
+		assertThatThrownBy(() -> paymentService.successPayment(PAYMENT_KEY, booking.getId(), actualAmount))
+			.isInstanceOf(BookingException.class)
+			.hasMessageContaining(BookingErrorCode.PAYMENT_AMOUNT_MISMATCH.getMessage());
+	}
+
+	@Test
+	public void 카드_결제를_취소한다() {
+		// given
+		LocalDateTime eventStartedAt = NOW.plusDays(2);
+		LocalDateTime eventEndedAt = NOW.plusDays(2).plusMinutes(120);
+		LocalDateTime bookingStartedAt = NOW;
+		LocalDateTime bookingEndedAt = NOW.plusDays(1);
+		LocalDateTime paymentRequestedAt = NOW.plusMinutes(2);
+
+		EventHall hall = EventHallFactory.generate();
+		eventHallRepository.save(hall);
+
+		Event event = EventFactory.generate(
+			hall,
+			eventStartedAt,
+			eventEndedAt,
+			bookingStartedAt,
+			bookingEndedAt
+		);
+		eventRepository.save(event);
+
+		EventTime time = EventTimeFactory.generate(event, eventStartedAt, eventEndedAt);
+		eventTimeRepository.save(time);
+
+		EventSeatArea area = EventSeatAreaFactory.generate(event, SeatAreaType.S);
+		eventSeatAreaRepository.save(area);
+
+		EventSeat seat = EventSeatFactory.generate(time, area, "A1", EventSeatStatus.AVAILABLE);
+		eventSeatRepository.save(seat);
+
+		Booking booking = BookingFactory.generate(
+			member,
+			time,
+			seat.getEventSeatArea().getPrice(),
+			BookingStatus.PAYMENT_COMPLETED
+		);
+		Ticket ticket = TicketFactory.generate(seat);
+		booking.addTicket(ticket);
+
+		Payment payment = PaymentFactory.generate(
+			PaymentMethod.CARD,
+			booking.getAmount(),
+			PaymentStatus.DONE
+		);
+		payment.updateConfirmInfo(PAYMENT_KEY, paymentRequestedAt);
+		booking.updatePayment(payment);
+		booking = bookingRepository.save(booking);
+
+		// when
+		paymentService.cancelPayment(PAYMENT_KEY, new PaymentCancelRequest(
+			"단순 변심",
+			payment.getAmount(),
+			Optional.empty()
+		));
+
+		// then
+		booking = bookingRepository.findBookingInfoById(booking.getId()).get();
+		payment = paymentRepository.findByPaymentKey(PAYMENT_KEY).get();
+
+		assertThat(payment.getStatus()).isEqualTo(PaymentStatus.CANCELED);
+		assertThat(booking.getStatus()).isEqualTo(BookingStatus.CANCELED);
+		assertThat(payment.getRefundAccountNumber()).isNull();
+	}
+
+	@Test
+	public void 가상계좌_결제를_진행한다() {
+		// given
+		LocalDateTime eventStartedAt = NOW.plusDays(2);
+		LocalDateTime eventEndedAt = NOW.plusDays(2).plusMinutes(120);
+		LocalDateTime bookingStartedAt = NOW;
+		LocalDateTime bookingEndedAt = NOW.plusDays(1);
+
+		EventHall hall = EventHallFactory.generate();
+		eventHallRepository.save(hall);
+
+		Event event = EventFactory.generate(
+			hall,
+			eventStartedAt,
+			eventEndedAt,
+			bookingStartedAt,
+			bookingEndedAt
+		);
+		eventRepository.save(event);
+
+		EventTime time = EventTimeFactory.generate(event, eventStartedAt, eventEndedAt);
+		eventTimeRepository.save(time);
+
+		EventSeatArea area = EventSeatAreaFactory.generate(event, SeatAreaType.S);
+		eventSeatAreaRepository.save(area);
+
+		EventSeat seat = EventSeatFactory.generate(time, area, "A1", EventSeatStatus.AVAILABLE);
+		eventSeatRepository.save(seat);
+
+		Booking booking = BookingFactory.generate(
+			member,
+			time,
+			seat.getEventSeatArea().getPrice(),
+			BookingStatus.WAITING_FOR_PAYMENT
+		);
+		Ticket ticket = TicketFactory.generate(seat);
+		booking.addTicket(ticket);
+		Payment payment = PaymentFactory.generate(
+			PaymentMethod.VIRTUAL_ACCOUNT,
+			booking.getAmount(),
+			PaymentStatus.READY
+		);
+		booking.updatePayment(payment);
+		bookingRepository.save(booking);
+
+		// when
+		PaymentSuccessResponse response = paymentService.successPayment(PAYMENT_KEY, booking.getId(),
+			booking.getAmount());
+
+		// then
+		payment = paymentRepository.findByPaymentKey(PAYMENT_KEY).get();
+		booking = bookingRepository.findWithPaymentById(booking.getId()).get();
+
+		assertThat(payment.getStatus()).isEqualTo(PaymentStatus.DONE);
+		assertThat(booking.getStatus()).isEqualTo(BookingStatus.WAITING_FOR_PAYMENT);
+
+		assertThat(payment.getAccountNumber()).isEqualTo(response.virtualAccount().accountNumber());
+		assertThat(payment.getBankCode()).isEqualTo(BankCode.getByBankNumCode(response.virtualAccount().bankCode()));
+		assertThat(payment.getDepositorName()).isEqualTo(response.virtualAccount().customerName());
+	}
 }
