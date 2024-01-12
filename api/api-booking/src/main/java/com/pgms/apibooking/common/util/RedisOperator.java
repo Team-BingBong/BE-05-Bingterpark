aa6207f26eda1d36f48296fae903fbeb8bb066a1
Hanna <hanna@trdst.com>
2024-01-12 19:58:03 +0900

    feat: Redis 명령어 래퍼 클래스 작성 -> RedisOperator

---
+++ api/api-booking/src/main/java/com/pgms/apibooking/common/util/RedisOperator.java
@@ -0,0 +1,59 @@
+package com.pgms.apibooking.common.util;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import com.pgms.apibooking.common.exception.BookingException;
+import com.pgms.coredomain.domain.common.BookingErrorCode;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RedisOperator {
+
+	private final RedisTemplate<String, String> redisTemplate;
+
+	public void setIfAbsent(String key, String value, Integer expirationSeconds) {
+		Duration timeout = Duration.ofSeconds(expirationSeconds);
+		tryOperation(() -> redisTemplate.opsForValue().setIfAbsent(key, value, timeout));
+	}
+
+	public String get(String key) {
+		return tryOperation(() -> redisTemplate.opsForValue().get(key));
+	}
+
+	public void delete(String key) {
+		tryOperation(() -> redisTemplate.delete(key));
+	}
+
+	public void addToZSet(String key, String value, double score) {
+		tryOperation(() -> redisTemplate.opsForZSet().add(key, value, score));
+	}
+
+	public Long getRankFromZSet(String key, String value) {
+		return tryOperation(() -> redisTemplate.opsForZSet().rank(key, value));
+	}
+
+	public void removeElementFromZSet(String key, String value) {
+		tryOperation(() -> redisTemplate.opsForZSet().remove(key, value));
+	}
+
+	public void removeRangeByScoreFromZSet(String key, double minScore, double maxScore) {
+		tryOperation(() -> redisTemplate.opsForZSet().removeRangeByScore(key, minScore, maxScore));
+	}
+
+	private <T> T tryOperation(Supplier<T> operation) {
+		try {
+			return operation.get();
+		} catch (Exception e) {
+			log.error("RedisManager occurred: " + e.getMessage(), e);
+			throw new BookingException(BookingErrorCode.SERVICE_UNAVAILABLE);
+		}
+	}
+}
--- core/core-domain/src/main/java/com/pgms/coredomain/domain/common/BookingErrorCode.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/common/BookingErrorCode.java
@@ -46,7 +46,8 @@ public enum BookingErrorCode implements BaseErrorCode{
 	// common
 	FORBIDDEN(HttpStatus.FORBIDDEN, "BOOKER_NOT_SAME", "권한이 없습니다."),
 	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "INVALID_INPUT_VALUE", "입력값을 확인해 주세요."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 에러");
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 에러"),
+	SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "SERVICE_UNAVAILABLE", "잠시 후 다시 시도해주세요.");
 
 	private final HttpStatus status;
 	private final String code;
